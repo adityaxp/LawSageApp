c3f88464d2041e3a9a9c8f148fcacb895a7b3ce2
Aditya <adityabalsane10@gmail.com>
2024-03-13 18:31:08 +0530

    fix(navigation): Fixed navigation errors

--- src/infrastructure/navigation/AppStack.jsx
+++ src/infrastructure/navigation/AppStack.jsx
@@ -3,6 +3,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { BottomTabNavigation } from "./BottomTabNavigation";
 import { ChatScreen } from "../../features/Chat/screens/ChatScreen";
+import { LoginScreen } from "../../features/Authentication/screens/LoginScreen";
+import { RegisterScreen } from "../../features/Authentication/screens/RegisterScreen";
+
 const Stack = createNativeStackNavigator();
 
 export const AppStack = () => {
@@ -18,6 +21,18 @@ export const AppStack = () => {
         <Stack.Screen
           name="ChatScreen"
           component={ChatScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="RegisterScreen"
+          component={RegisterScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
